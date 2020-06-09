7446b49deb4f1acd7f7931d8551a8148fefc6826
jairmaiag <jairmaiag@gmail.com>
2020-06-09 15:55:14 -0300

    Criação do modelo e do migration da tabela usuário

--- models/usuario.js
+++ models/usuario.js
@@ -3,35 +3,65 @@ module.exports = (sequelize, DataTypes) => {
   const tabela = "usuario";
   const campos = {
     id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
       type: DataTypes.INTEGER,
       field: "usuid",
-      primaryKey: true,
-      autoIncrement: true,
+      comment: "Id na tabela, identificando registro único.",
     },
     login: {
-      type: DataTypes.STRING,
-      field: "usulogin",
+      type: DataTypes.STRING(30),
       allowNull: false,
+      field: "usulogin",
+      comment: "Login do usuáiro para acesso ao sistema.",
     },
     senha: {
-      type: DataTypes.STRING,
-      field: "ususenha",
+      type: DataTypes.STRING(64),
       allowNull: false,
+      field: "ususenha",
+      comment: "Senha do usuáiro para acesso ao sistema.",
+    },
+    ciracao: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+      field: "usucriacao",
+      comment: "Data de criação do usuáiro para acesso ao sistema.",
+    },
+    expira: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+      field: "usuexpirar",
+      comment: "Data de expiração do usuáiro para acesso ao sistema.",
+    },
+    inativo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+      field: "usuinativo",
+      comment: "Indica se o usuáiro está inativo para acesso ao sistema.",
+    },
+    idPessoa: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      field: "pesid",
+      comment: "Id da tabela Pessoa, onde este usuário pertence.",
     },
   };
-  /* Definição de opções da entidade */
+  /* Definição de opções da entidade (tabela) */
   const opcoes = {
-    modelName: tabela,
     tableName: tabela,
-    timestamps: false,
+    comment:
+      "Tabela utilizada para armazenar os dados do usuário para acesso ao sistema.", // comment for table
   };
-  var Usuario = sequelize.define("Usuario", campos, opcoes);
+
+  const Usuario = sequelize.define("Usuario", campos, opcoes);
   Usuario.associate = function (models) {
     /* Usuário pertence a (belongsTo) Pessoa */
     Usuario.belongsTo(models.Pessoa, {
       as: "Pessoa",
       foreignKey: "pesid",
-      allowNull: true,
+      allowNull: false,
     });
   };
   return Usuario;
