fd960dfbcd70fe44454b175e6587e1c968947021
jairmaiag <jairmaiag@gmail.com>
2020-06-09 13:25:22 -0300

    Nova forma de chamar os registros

--- src/app/route/usuarioRoute.js
+++ src/app/route/usuarioRoute.js
@@ -1,11 +1,12 @@
 module.exports = function (app) {
   var controller = new app.src.app.controller.usuario.UsuarioController(app);
   var util = new app.src.app.server.Util(app);
+
   app.get("/usuario", async function (req, res) {
-    controller.index(req.query.filter).then((pessoas) => {
-      let total = pessoas.length;
+    controller.index(req.query.filter).then((entiys) => {
+      let total = entiys.length;
       if (total > 0) {
-        res.status(200).json(pessoas);
+        res.status(200).json(entiys);
       } else {
         res
           .status(404)
@@ -19,23 +20,28 @@ module.exports = function (app) {
   });
 
   app.get("/usuario/:id", async function (req, res) {
-    await controller.findById(req.params.id).then((pessoa) => {
-      if (!Object.is(pessoa, null)) {
-        res.status(200).json(pessoa);
+    await controller.findById(req.params.id).then((entity) => {
+      let isNull = Object.is(entity, null);
+      if (!isNull) {
+        res.status(201).json(entity);
       } else {
         res
           .status(404)
           .json(util.montarMensagemJson("Recurso não encontrado."));
       }
+      return;
     });
   });
 
   app.post("/usuario", async function (req, res) {
     await controller.insert(req.body).then((entity) => {
-      if (!Object.is(entity, null)) {
-        res.status(200).json(entity);
+      if (entity !== "null") {
+        /* 201 Created 
+           A requisição foi bem sucedida e um novo recurso foi criado como resultado
+        */
+        res.status(201).json(entity);
       } else {
-        res.status(404).json(util.montarMensagemJson("Recurso não criado."));
+        res.status(404).json(util.montarMensagemJson("Recurso não incluido."));
       }
     });
   });
@@ -43,31 +49,37 @@ module.exports = function (app) {
   app.put("/usuario", async function (req, res) {
     let id = req.body.id;
     if (id === undefined) {
+      /* 400 Bad Request
+         Essa resposta significa que o servidor não entendeu a requisição pois está com uma sintaxe inválida.
+      */
       res
-        .status(404)
+        .status(400)
         .json(util.montarMensagemJson("Envio do campo Id é obrigatório."));
       return;
     }
-    let entity = controller.update(req.body);
-    if (entity !== "null") {
-      res.status(200).json(entity);
-    } else {
-      res
-        .status(404)
-        .json(
-          util.montarMensagemJson(
-            "Registro não encontrado para ser atualizado."
-          )
-        );
-    }
+    await controller.update(req.body).then((entity) => {
+      if (entity[0] === 1) {
+        res.redirect("/usuario/" + id);
+      } else {
+        res
+          .status(404)
+          .json(
+            util.montarMensagemJson(
+              "Recurso não encontrado para ser atualizado."
+            )
+          );
+      }
+    });
   });
 
   app.delete("/usuario/:id", async function (req, res) {
     let qtdRemovido = await controller.delete(req.params.id);
     if (qtdRemovido !== "0") {
-      res.status(200).json(util.montarMensagemJson("Total removido: " + qtdRemovido));
+      res
+        .status(200)
+        .json(util.montarMensagemJson("Total removido: " + qtdRemovido));
     } else {
-      res.status(404).json(util.montarMensagemJson("Registro não encontrado."));
+      res.status(404).json(util.montarMensagemJson("Recurso não encontrado."));
     }
   });
 };
