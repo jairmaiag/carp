7649cce1b6298303975e4514920ddaf2afc184ad
jairmaiag <jairmaiag@gmail.com>
2020-06-10 16:46:35 -0300

    Exibição das instruções

--- src/app/route/mianRoute.js
+++ src/app/route/mianRoute.js
@@ -21,11 +21,14 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     controller.index(req, res);
   });
+  app.get("/criarbanco", function (req, res) {
+    res.status(200).send('Passe os dados utilizando o method POST, no formato json como abaixo: <br />{<br />"host":"enderecoBanco", <br />"porta":5432, <br />"banco":"postgres",<br />"usuario":"postgres", <br />"senha":"senhaBanco" <br />} <br />Substituindo os dados para a conexão com o banco padrão do Postgres.');
+  });
   app.post("/criarbanco", async function (req, res) {
     if (!validaCampos(req.body)) {
       res.status(400).json({
         mensagem:
-          "Formato esperado como no exemplo: {'host':'enderecoBanco', 'porta':5432,'banco':'postgres','usuario':'postgres','senha':'senhaBanco'} - Substituindo as aspas simples por aspas duplas.",
+          "Formato esperado como no exemplo: {\"host\":'enderecoBanco', 'porta':5432,'banco':'postgres','usuario':'postgres','senha':'senhaBanco'} - Substituindo as aspas simples por aspas duplas.",
       });
       return;
     }
