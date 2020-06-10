a5d2f86f8988d2fd61cb2d8cd92f7fd15207e505
jairmaiag <jairmaiag@gmail.com>
2020-06-10 15:25:49 -0300

    Exibindo mensagens de utilização no console.

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
 var app = require("./src/app/config/server");
 
 /* parametrizar a porta de escuta */
-app.listen(80, function () {
-  console.log("Servidor online");
+let port = 80;
+app.listen(port, function () {
+  console.clear();
+  console.log("Servidor online na port: " + port);
+  console.log("Acesse http://localhost");
+  console.log(
+    "Para criar o banco acesse http://localhost/criarbanco e siga as instruções."
+  );
 });
 
 /* Utilizar o comando abaixo para sincronizar o banco, criando as tabelas. 
