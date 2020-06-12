796af34f1bfc0952c7c20e2fc15e2193799126f9
jairmaiag <jairmaiag@gmail.com>
2020-06-12 18:11:59 -0300

    Criação do método de busca do usuário pelo Login e Senha

--- src/app/repository/UsuarioRepository.js
+++ src/app/repository/UsuarioRepository.js
@@ -72,6 +72,23 @@ UsuarioRepository.prototype.delete = async function (id) {
   }
 };
 
+UsuarioRepository.prototype.findByLoginSenha = async function (filter) {
+  try {
+    const result = await Usuario.findOne(
+      {
+        where: {
+          login: filter.login,
+          senha: filter.senha,
+        },
+      },
+      { raw: true }
+    );
+    return result;
+  } catch (e) {
+    throw e;
+  }
+};
+
 function retorno(app) {
   return new UsuarioRepository(app);
 }
