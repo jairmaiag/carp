cce45c872ccb9f2e9fbb58134a249c54c67eafb7
jairmaiag <jairmaiag@gmail.com>
2020-06-15 09:39:58 -0300

    Criação da sessão com obejto usuario e criação da rota de logout para finalizar a sessão.

--- src/app/route/acessoRoute.js
+++ src/app/route/acessoRoute.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
 
   app.post("/login", async function (req, res) {
     const filter = req.body;
-    console.log(filter);
     if (
       Object.is(filter.login, undefined) ||
       Object.is(filter.senha, undefined)
@@ -20,7 +19,12 @@ module.exports = function (app) {
     if (usuario == null) {
       res.status(404).json(util.montarMensagemJson("Recurso não encontrado."));
     } else {
+      req.session.usuario = usuario;
       res.status(200).json(usuario);
     }
+  });
+  app.get("/logout", async function (req, res) {
+    req.session.destroy();
+    res.status(200).json(util.montarMensagemJson("Usuário não logado."));
   });
 };
