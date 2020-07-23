91137dcbf1e251a84a5622ba6a337c1170b53c68
jairmaiag <jairmaiag@gmail.com>
2020-07-23 14:17:52 -0300

    Inclusão do UUId no model de Pessoa

--- src/app/usecase/UsuarioUC.js
+++ src/app/usecase/UsuarioUC.js
@@ -1,56 +1,86 @@
-const UUIDGenerator = require('../util/UUIDGenerator')
+const UUIDGenerator = require("../util/UUIDGenerator");
 
 class UsuarioUC {
   constructor(app) {
-    this.app = app
-    this.repository = null
+    this.app = app;
+    this.repository = null;
   }
 
   criarRepository() {
-    return (this.repository = new this.app.src.app.repository.UsuarioRepository(this.app))
+    return (this.repository = new this.app.src.app.repository.UsuarioRepository(
+      this.app
+    ));
   }
 
-  async index(attributes, filter, order){
-    this.repository = this.criarRepository()
-    return await this.repository.findAll(attributes, filter, order)
+  async index(attributes, filter, order) {
+    this.repository = this.criarRepository();
+    return await this.repository.findAll(attributes, filter, order);
   }
 
-  async findAndPaginate(attributes, filter, order, page){
-    this.repository = this.criarRepository()
-    return await this.repository.findAndPaginate(attributes, filter, order, page)
+  async findAndPaginate(attributes, filter, order, page) {
+    this.repository = this.criarRepository();
+    return await this.repository.findAndPaginate(
+      attributes,
+      filter,
+      order,
+      page
+    );
   }
 
   async findByUUId(UUId) {
-    this.repository = this.criarRepository()
-    return await this.repository.findByUUId(UUId)
+    this.repository = this.criarRepository();
+    return await this.repository.findByUUId(UUId);
   }
 
   async findById(id) {
-    this.repository = this.criarRepository()
-    return await this.repository.findById(id)
+    this.repository = this.criarRepository();
+    return await this.repository.findById(id);
   }
 
   async insert(dados) {
     if (!dados.UUId) {
-      dados.UUId = UUIDGenerator.getUUIDV4()
+      dados.UUId = UUIDGenerator.getUUIDV4();
     }
-    this.repository = this.criarRepository()
-    return await this.repository.insert(dados)
+    let nomePessoa = dados.login;
+    let sobrenomePessoa = null;
+    let uuidPessoa = UUIDGenerator.getUUIDV4();
+
+    if (dados.Pessoa) {
+      if (!dados.Pessoa.UUId) {
+        dados.Pessoa.UUId = uuidPessoa;
+      }
+      if (dados.Pessoa.nome) {
+        nomePessoa = dados.Pessoa.nome;
+      }
+      if (dados.Pessoa.sobrenome) {
+        sobrenomePessoa = dados.Pessoa.sobrenome;
+      }
+    } else {
+      dados.Pessoa = {
+        nome: nomePessoa,
+        ativo: dados.ativo,
+        sobrenome: sobrenomePessoa,
+        UUId: uuidPessoa,
+      };
+    }
+
+    this.repository = this.criarRepository();
+    return await this.repository.insert(dados);
   }
 
   async update(dados) {
-    this.repository = this.criarRepository()
-    return await this.repository.update(dados)
+    this.repository = this.criarRepository();
+    return await this.repository.update(dados);
   }
 
   async delete(UUId) {
-    this.repository = this.criarRepository()
-    return await this.repository.delete(UUId)
+    this.repository = this.criarRepository();
+    return await this.repository.delete(UUId);
   }
 }
 
 function retorno(app) {
-  return new UsuarioUC(app)
+  return new UsuarioUC(app);
 }
 
-module.exports = () => retorno
+module.exports = () => retorno;
