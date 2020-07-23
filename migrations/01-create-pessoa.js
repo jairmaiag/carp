13cc7822b7eed740b9c43a2260e8d62fb52f9095
jairmaiag <jairmaiag@gmail.com>
2020-07-23 10:41:41 -0300

    Inclusão da coluna UUId na tabela de pessoa

--- migrations/01-create-pessoa.js
+++ migrations/01-create-pessoa.js
@@ -1,81 +1,87 @@
-'use strict'
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable(
-      'Pessoa', 
+      "Pessoa",
       {
+        UUId: {
+          field: "UUIdPes",
+          allowNull: false,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+        },
         id: {
-          field: 'idPes',
+          field: "idPes",
           primaryKey: true,
           autoIncrement: true,
           type: Sequelize.INTEGER,
-          comment: 'Id na tabela, identificando registro único.',
+          comment: "Id na tabela, identificando registro único.",
         },
         nome: {
-          field: 'nomePes',
+          field: "nomePes",
           allowNull: false,
           type: Sequelize.STRING(20),
-          comment: 'Nome da pessoa cadastrada.',
+          comment: "Nome da pessoa cadastrada.",
         },
         nomeMeio: {
-          field: 'nomeMeioPes',
+          field: "nomeMeioPes",
           type: Sequelize.STRING(20),
-          comment: 'Nome do meio da pessoa cadastrada.',
+          comment: "Nome do meio da pessoa cadastrada.",
         },
         sobrenome: {
-          field: 'sobrenomePes',
+          field: "sobrenomePes",
           type: Sequelize.STRING(20),
-          comment: 'Sobre Nome da pessoa cadastrada.',
+          comment: "Sobre Nome da pessoa cadastrada.",
         },
         nascimento: {
-          field: 'nascimentoPes',
+          field: "nascimentoPes",
           type: Sequelize.DATEONLY,
-          comment: 'Data de nascimento da pessoa cadastrada.',
+          comment: "Data de nascimento da pessoa cadastrada.",
         },
         sexo: {
-          field: 'sexoPes',
-          defaultValue: 'F',
+          field: "sexoPes",
+          defaultValue: "F",
           type: Sequelize.CHAR(1),
-          comment: 'Sexo da pessoa cadastrada podendo assumir valores F ou M.',
+          comment: "Sexo da pessoa cadastrada podendo assumir valores F ou M.",
         },
         cpf: {
-          field: 'cpfPes',
+          field: "cpfPes",
           type: Sequelize.STRING(11),
-          comment: 'Número do CPF da pessoa cadastrada, somente números.',
+          comment: "Número do CPF da pessoa cadastrada, somente números.",
         },
         rg: {
-          field: 'rgPes',
+          field: "rgPes",
           type: Sequelize.STRING(9),
-          comment: 'Número do RG da pessoa cadastrada, somente números.',
+          comment: "Número do RG da pessoa cadastrada, somente números.",
         },
         ativo: {
-          field: 'ativoPes',
+          field: "ativoPes",
           allowNull: false,
           defaultValue: false,
           type: Sequelize.BOOLEAN,
-          comment: 'Indica se a pessoa está ativa para ser utilizada.',
+          comment: "Indica se a pessoa está ativa para ser utilizada.",
         },
         createdAt: {
-          field: 'createAtPes',
+          field: "createAtPes",
           allowNull: false,
           type: Sequelize.DATE,
         },
         updatedAt: {
-          field: 'updatedAtPes',
+          field: "updatedAtPes",
           allowNull: false,
           type: Sequelize.DATE,
         },
       },
       {
-        schema: 'carp', // default: public, PostgreSQL only.
+        schema: "carp", // default: public, PostgreSQL only.
         comment:
-          'Tabela utilizada para armazenar os dados de pessoas do sistema.',
+          "Tabela utilizada para armazenar os dados de pessoas do sistema.",
       }
-    )
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Pessoa')
+    return queryInterface.dropTable("Pessoa");
   },
-}
+};
