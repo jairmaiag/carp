7080c122f6ff1306d927fbb701d107696703236e
Itamar Nunes <itamarbel@gmail.com>
2020-07-30 21:09:32 -0300

    refactor: remove PessoaUC

--- src/app/repository/PessoaRepository.js
+++ src/app/repository/PessoaRepository.js
@@ -1,12 +1,8 @@
-const { Usuario, Pessoa } = require("../../../models");
+const { Usuario, Pessoa } = require('../../../models')
 
-var PessoaRepository = function (app) {};
+var PessoaRepository = function (app) { }
 
-PessoaRepository.prototype.findAll = async function (
-  attributes,
-  filter,
-  order
-) {
+PessoaRepository.prototype.findAll = async function (attributes, filter, order) {
   try {
     const result = await Pessoa.findAll({
       attributes: attributes,
@@ -15,88 +11,83 @@ PessoaRepository.prototype.findAll = async function (
       order: order || [["id", "ASC"]],
       include: [{ model: Usuario, as: "Usuario" }],
       raw: true,
-    });
+    })
 
-    return result;
+    return result
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
-PessoaRepository.prototype.findAndPaginate = async function (
-  attributes,
-  filter,
-  order,
-  page
-) {
+PessoaRepository.prototype.findAndPaginate = async function (attributes, filter, order, page) {
   try {
-    page = await Pessoa.findAndPaginate(attributes, filter, order, page);
+    page = await Pessoa.findAndPaginate(attributes, filter, order, page)
 
-    return page;
+    return page
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 PessoaRepository.prototype.findById = async function (id) {
   try {
     const result = await Pessoa.findByPk(id, {
       include: [{ model: Usuario, as: "Usuario" }],
-    });
+    })
 
-    return result;
+    return result
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 PessoaRepository.prototype.findByUUId = async function (UUId) {
   try {
     const result = await Pessoa.findOne({
       where: { UUId: UUId },
       include: [{ model: Usuario, as: "Usuario" }],
-    });
+    })
 
-    return result;
+    return result
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 PessoaRepository.prototype.insert = async function (dados) {
   try {
-    const result = await Pessoa.create(dados);
+    const result = await Pessoa.create(dados)
 
-    return result;
+    return result
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 PessoaRepository.prototype.update = async function (dados) {
   try {
-    const result = await Pessoa.update(dados, { where: { id: dados.id } });
+    const result = await Pessoa.update(dados, { where: { id: dados.id } })
 
-    const retorno = result[0] === 1 ? 1 : null;
+    const retorno = result[0] === 1 ? 1 : null
 
-    return retorno;
+    return retorno
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 PessoaRepository.prototype.delete = async function (id) {
   try {
     const result = await Pessoa.destroy({
       where: { id: id },
-    });
+    })
 
-    return result;
+    return result
   } catch (error) {
-    throw error;
+    throw error
   }
-};
-
-function retorno(app) {
-  return new PessoaRepository(app);
 }
 
-module.exports = () => retorno;
+function retorno(app) {
+  return new PessoaRepository(app)
+}
+
+module.exports = () => retorno
