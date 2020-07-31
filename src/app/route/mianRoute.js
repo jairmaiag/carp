651954247a2de2f76d7b6f19e5b6d5dc42c8f34e
Itamar Nunes <itamarbel@gmail.com>
2020-07-30 22:08:31 -0300

    refactor: remove MainUC

--- src/app/route/mianRoute.js
+++ src/app/route/mianRoute.js
@@ -1,47 +1,52 @@
-const PDFDocument = require("pdfmake");
+const PDFDocument = require("pdfmake")
+
 module.exports = function (app) {
-  const controller = new app.src.app.controller.main.MainController(app);
+  const controller = new app.src.app.controller.main.MainController(app)
+
   const validaCampos = function (dados) {
     if (dados.host === undefined) {
-      return false;
+      return false
     }
     if (dados.porta === undefined) {
-      return false;
+      return false
     }
     if (dados.banco === undefined) {
-      return false;
+      return false
     }
     if (dados.usuario === undefined) {
-      return false;
+      return false
     }
     if (dados.senha === undefined) {
-      return false;
+      return false
     }
-    return true;
-  };
+    return true
+  }
+
   app.get("/", function (req, res) {
-    controller.index(req, res);
-  });
+    controller.index(req, res)
+  })
+
   app.get("/criarbanco", function (req, res) {
-    res
-      .status(200)
-      .send(
-        'Passe os dados utilizando o method POST, no formato json como abaixo: <br />{<br />"host":"enderecoBanco", <br />"porta":5432, <br />"banco":"postgres",<br />"usuario":"postgres", <br />"senha":"senhaBanco" <br />} <br />Substituindo os dados para a conexão com o banco padrão do Postgres.'
-      );
-  });
+    res.status(200).send(
+      'Passe os dados utilizando o method POST, no formato json como abaixo: <br />{<br />"host":"enderecoBanco", <br />"porta":5432, <br />"banco":"postgres",<br />"usuario":"postgres", <br />"senha":"senhaBanco" <br />} <br />Substituindo os dados para a conexão com o banco padrão do Postgres.')
+  })
+
   app.post("/criarbanco", async function (req, res) {
     if (!validaCampos(req.body)) {
       res.status(400).json({
         mensagem:
           "Formato esperado como no exemplo: {\"host\":'enderecoBanco', 'porta':5432,'banco':'postgres','usuario':'postgres','senha':'senhaBanco'} - Substituindo as aspas simples por aspas duplas.",
-      });
-      return;
+      })
+      return
     }
-    const retorno = await controller.criarBanco(req.body);
+
+    await controller.criarBanco(req.body)
+
     res.status(200).json({
       mensagem: "Verifique o console do nodejs para ver os resultados.",
-    });
-  });
+    })
+  })
+
   app.get("/teste.pdf", function (req, res) {
     var fonts = {
       Roboto: {
@@ -50,7 +55,7 @@ module.exports = function (app) {
         italics: "fonts/Roboto-Italic.ttf",
         bolditalics: "fonts/Roboto-MediumItalic.ttf",
       },
-    };
+    }
     var docDefinition = {
       info: {
         title: "Teste de geração de PDF",
@@ -69,10 +74,10 @@ module.exports = function (app) {
       styles: {
         header: { fontSize: 30, bold: true },
       },
-    };
-    const doc = new PDFDocument(fonts);
-    const pdfDoc = doc.createPdfKitDocument(docDefinition);
-    pdfDoc.pipe(res);
-    pdfDoc.end();
-  });
-};
+    }
+    const doc = new PDFDocument(fonts)
+    const pdfDoc = doc.createPdfKitDocument(docDefinition)
+    pdfDoc.pipe(res)
+    pdfDoc.end()
+  })
+}
