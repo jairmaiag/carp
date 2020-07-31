8fa1401bb89fc698cb90d24fc0ee9b46ba259796
Itamar Nunes <itamarbel@gmail.com>
2020-07-30 22:41:19 -0300

    refactor: move server.js to app folder

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* importar as configurações do servidor */
-var app = require("./src/app/config/server")
+var app = require("./src/app/server")
 
 /* parametrizar a porta de escuta */
 let port = process.env.PORT || 80
@@ -7,7 +7,5 @@ app.listen(port, function () {
   console.log(`\nServidor online na port: ${port}`)
   let acesso = "Acesse http://localhost" + (port == 80 ? "" : ":" + port)
   console.log(acesso)
-  console.log(
-    `Para criar o banco acesse ${acesso}/criarbanco e siga as instruções.\n`
-  )
+  console.log(`Para criar o banco acesse ${acesso}/criarbanco e siga as instruções.\n`)
 })
