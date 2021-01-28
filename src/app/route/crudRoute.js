f656c7e3cd7255cef681ad57648d89493a438621
Jair <jairmaiag@gmail.com>
2021-01-28 16:36:26 -0300

    Inclusão do middlwere

--- src/app/route/crudRoute.js
+++ src/app/route/crudRoute.js
@@ -2,18 +2,19 @@ const adaptRoute = require('../adapters/express/ExpressRouteAdapter')
 const checkUUId = require('../middleware/CheckUUIdMiddlewareFactory')
 const checkId = require('../middleware/CheckIdMiddlewareFactory')
 const checkFilter = require('../middleware/CheckFilterMiddlewareFactory')
+const checkLogin = require('../middleware/CheckLoginMiddleware')
 
 module.exports = function (app) {
-  app.get('/:model/', checkFilter, adaptRoute(app))
-  app.get('/:model/paginacao', adaptRoute(app))
-  app.get('/:model/UUId/:UUId', checkUUId, adaptRoute(app))
-  app.get('/:model/:id', checkId, adaptRoute(app))
+  app.get('/:model/', checkLogin, checkFilter, adaptRoute(app))
+  app.get('/:model/paginacao', checkLogin, adaptRoute(app))
+  app.get('/:model/UUId/:UUId',checkLogin, checkUUId, adaptRoute(app))
+  app.get('/:model/:id',checkLogin, checkId, adaptRoute(app))
   
-  app.post('/:model/', checkUUId, adaptRoute(app))
+  app.post('/:model/',checkLogin, checkUUId, adaptRoute(app))
 
-  app.put('/:model/', checkUUId, adaptRoute(app))
+  app.put('/:model/',checkLogin, checkUUId, adaptRoute(app))
 
-  app.delete('/:model/UUId/:UUId', checkUUId, adaptRoute(app))
+  app.delete('/:model/UUId/:UUId',checkLogin, checkUUId, adaptRoute(app))
 
   // Delete por ID provavelmente não vai existir
   // app.delete('/:model/:id', checkId, adaptRoute(app))
