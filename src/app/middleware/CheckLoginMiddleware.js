0c872851dc78fd95f6e68528bb932054d3bb4cee
Jair <jairmaiag@gmail.com>
2021-01-28 11:52:22 -0300

    Criação do middleware de checagem de login

---
+++ src/app/middleware/CheckLoginMiddleware.js
@@ -0,0 +1,15 @@
+CheckLoginMiddleware = (req, res, next) =>{
+    if (process.env.NODE_ENV === 'production') {
+        let urlOrigem = req.originalUrl
+        if (urlOrigem === '/' || urlOrigem === '/login') {
+          return next();
+        }
+        if (req.session.usuario === undefined) {
+          res.status(401).json({mensagem: `${req.i18n_texts.user_not_logged_in} ${req.i18n_texts.please_try_again}`})
+          return;
+        }
+      }
+      next();
+};
+
+module.exports = CheckLoginMiddleware
