a8995e3d1ca616177bb600e802be859f379ee72a
Jair <jairmaiag@gmail.com>
2021-01-29 09:44:51 -0300

    Criação do model Pefil

---
+++ src/db/models/perfil.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const BaseModel = require('./basemodel')
+
+module.exports = (sequelize, DataTypes) => {
+  class Perfil extends BaseModel { }
+
+  Perfil.init({
+    UUId: {
+      field: 'UUIdPer',
+      allowNull: false,
+      type: DataTypes.UUID,
+      comment: 'UUID do perfil, utilizado para acessar os dados.',
+      validate: {
+        notNull: true
+      },
+    },
+    id: {
+      field: 'idPer',
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      comment: 'Id na tabela, identificando registro único.',
+    },
+    nome: {
+        field: 'nomePer',
+        allowNull: false,
+        type: DataTypes.STRING(30),
+        comment: 'Nome do perfil.',
+    },
+    descricao: {
+        field: 'descricaoPer',
+        type: DataTypes.TEXT,
+        comment: 'Descrição do perfil.',
+    },
+    ativo: {
+      field: 'ativoPer',
+      allowNull: false,
+      defaultValue: true,
+      type: DataTypes.BOOLEAN,
+      comment: 'Indica se o Perário está ativo para acesso ao sistema.',
+    },
+    idPerario: {
+      field: 'idPer',
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      comment: 'Id da tabela Perario, onde este perfil pertence.',
+    },
+    createdAt: {
+      field: 'createAtPer',
+      allowNull: false,
+      type: DataTypes.DATE,
+      comment: 'Data de ciração do registro.'
+    },
+    updatedAt: {
+      field: 'updatedAtPer',
+      allowNull: false,
+      type: DataTypes.DATE,
+      comment: 'Data de atualização do registro.'
+    }
+  }, {
+    sequelize,
+    freezeTableName: true,
+    timestamps: true,
+    modelName: 'Perfil',
+    comment: 'Tabela utilizada para armazenar os dados de perfil do Perário.'
+  })
+
+  Perfil.associate = function (models) {
+    /* Perário pertence a (belongsTo) Pessoa */
+    Perfil.belongsTo(models.Usuario, {
+      as: 'Usuario',
+      foreignKey: 'idPer',
+      allowNull: false,
+    });
+  };
+
+  return Perario
+}
