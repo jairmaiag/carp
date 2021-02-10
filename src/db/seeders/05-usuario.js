dcdee03f9ceac3b2c7e05db10a406ec5a643e43b
Jair <jairmaiag@gmail.com>
2021-02-10 19:30:15 -0300

    Nova ordem na inclusão de registros de Usuário

---
+++ src/db/seeders/05-usuario.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.bulkInsert('Usuario', [{
+      loginUsu: 'admin@admin.com',
+      senhaUsu:'admin',
+      idPes:1,
+      createAtUsu: new Date(),
+      updatedAtUsu: new Date()
+   }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Pessoa', null, {});
+  }
+};
