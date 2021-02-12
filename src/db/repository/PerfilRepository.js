1ada2c024f201493be4ca7c50f791957004e79ec
Jair <jairmaiag@gmail.com>
2021-02-12 20:08:03 -0300

    Inclusão e alteração de perfil com recursos

--- src/db/repository/PerfilRepository.js
+++ src/db/repository/PerfilRepository.js
@@ -1,7 +1,8 @@
 const { Perfil, Recurso, RecursoPerfil } = require("../models")
+const util = require('../../app/util/Util')
 
 const PerfilRepository = function () { }
-const include = { association: 'Recursos',through: {attributes: [] } };
+const include = { association: 'Recursos', through: { attributes: [] } };
 
 PerfilRepository.prototype.findAll = async function (attributes, filter, order) {
   const result = await Perfil.findAll({
@@ -24,25 +25,22 @@ PerfilRepository.prototype.findAndPaginate = async function (attributes, filter,
   return page
 }
 PerfilRepository.prototype.findById = async function (id) {
-  return await Perfil.findByPk(id,include);
+  return await Perfil.findByPk(id, include);
 }
 PerfilRepository.prototype.findByUUId = async function (UUId) {
-  let perfis = await this.findAll({},{ UUId: UUId });
-  return perfis.length === 0? null: perfis[0];
+  return Perfil.findOne({ where: { UUId: UUId }, include });
 }
 
 PerfilRepository.prototype.insert = async function (dados) {
   let perfil = await Perfil.create(dados);
-  await this.insertRecursos(dados.Recursos, perfil);
-  return perfil;
+  return await this.manutencaoRecursos(dados.Recursos, perfil);
 }
 
 PerfilRepository.prototype.update = async function (dados) {
   const result = await Perfil.update(dados, { where: { UUId: dados.UUId } })
   if (result[0] === 1) {
     let perfil = await this.findByUUId(dados.UUId);
-    await this.insertRecursos(dados.Recursos, perfil);
-    return perfil;
+    return await this.manutencaoRecursos(dados.Recursos, perfil);
   } else {
     return null;
   }
@@ -52,16 +50,18 @@ PerfilRepository.prototype.delete = async function (UUId) {
   return await Perfil.destroy({ where: { UUId: UUId }, });
 }
 
-PerfilRepository.prototype.insertRecursos = async function (dados, perfil) {
-  if (dados && dados.length > 0) {
-    for (let i = 0; i < dados.length; i++) {
-      let recursoModel = await Recurso.findOne({ where: { UUId: dados[i].UUId } });
-      if (recursoModel) {
-        await perfil.reload();
-        await perfil.addRecursos(recursoModel);
-      }
-    }
+PerfilRepository.prototype.manutencaoRecursos = async function (dados, perfil) {
+  let recursosBack = perfil.Recursos.map(r => r.dataValues);
+  let listas = await util.montarListasExclusaoInclusao(dados, recursosBack);
+  if (listas.listaIncluir.length !== 0) {
+    let litaIncluir = listas.listaIncluir.map(rec => { return { RecursoId: rec.id, PerfilId: perfil.id } });
+    await RecursoPerfil.bulkCreate(litaIncluir);
   }
+  if (listas.listaExcluir.length !== 0) {
+    let litaExcluir = listas.listaExcluir.map(rec => rec.id);
+    await RecursoPerfil.destroy({ where: { PerfilId: perfil.id, RecursoId: litaExcluir } });
+  }
+  return await this.findByUUId(perfil.UUId);
 }
 
 module.exports = new PerfilRepository();
