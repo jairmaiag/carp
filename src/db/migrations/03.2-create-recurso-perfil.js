c3cb16b39635abf51ef4921d1670d30b989c55cb
Jair <jairmaiag@gmail.com>
2021-02-15 17:48:03 -0300

    Formatação de código

--- src/db/migrations/03.2-create-recurso-perfil.js
+++ src/db/migrations/03.2-create-recurso-perfil.js
@@ -1,36 +1,30 @@
-'use strict'
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable(
-      'RecursoPerfil', 
-      {
-        PerfilId: {
-          field: 'PerfilId',
-          type: Sequelize.INTEGER,
-          references: { model: 'Perfil', key: 'id' },
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE',
-          comment: 'Id na tabela de perfil.',
-        },
-        RecursoId: {
-            field: 'RecursoId',
-            type: Sequelize.INTEGER,
-            references: { model: 'Recurso', key: 'id' },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE',
-            comment: 'Id na tabela de recurso.',
-        }
+  up: (queryInterface, Sequelize) => queryInterface.createTable(
+    'RecursoPerfil',
+    {
+      PerfilId: {
+        field: 'PerfilId',
+        type: Sequelize.INTEGER,
+        references: { model: 'Perfil', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        comment: 'Id na tabela de perfil.',
       },
-      {
-        schema: 'carp', // default: public, PostgreSQL only.
-        comment:
-          'Tabela utilizada para armazenar a ligação dos recursos com os perfis.',
-      }
-    )
-  },
+      RecursoId: {
+        field: 'RecursoId',
+        type: Sequelize.INTEGER,
+        references: { model: 'Recurso', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        comment: 'Id na tabela de recurso.',
+      },
+    },
+    {
+      schema: 'carp', // default: public, PostgreSQL only.
+      comment:
+        'Tabela utilizada para armazenar a ligação dos recursos com os perfis.',
+    },
+  ),
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('RecursoPerfil')
-  },
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('RecursoPerfil'),
 };
