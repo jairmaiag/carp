7eb8411c4b8ff9b574d164efc2e87f5b49a52892
Jair <jairmaiag@gmail.com>
2021-02-15 17:46:57 -0300

    Criação do índice de unicidade pelo nome,nomeMeio e sobrenome

--- src/db/migrations/04.1-alter-pessoa.js
+++ src/db/migrations/04.1-alter-pessoa.js
@@ -1,20 +1,10 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ALTER COLUMN "UUIdPes" SET DEFAULT uuid_generate_v4()', { transaction: t })
-      ])
-    })
-  },
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ALTER COLUMN "UUIdPes" SET DEFAULT uuid_generate_v4()', { transaction: t }),
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesUnique" UNIQUE ("nomePes", "nomeMeioPes", "sobrenomePes")', { transaction: t }),
+  ])),
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        //queryInterface.sequelize.query('ALTER TABLE Pessoa ALTER COLUMN "UUIdPes" SET DEFAULT uuid_generate_v4()', { transaction: t })
-      ])
-    })
-  }
+  down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
+    // queryInterface.sequelize.query('ALTER TABLE Pessoa ALTER COLUMN "UUIdPes" SET DEFAULT uuid_generate_v4()', { transaction: t })
+  ])),
 };
-
