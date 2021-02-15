787271898e34ab8f2159277b03032836fc49b199
Jair <jairmaiag@gmail.com>
2021-02-15 15:43:49 -0300

    Configuração do ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
     node: true,
   },
   extends: [
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 7,
     sourceType: 'module',
   },
   rules: {
