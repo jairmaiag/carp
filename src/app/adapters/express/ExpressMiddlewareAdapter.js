dd7dba1f1c100b060692afa7888105700b6ab454
Jair <jairmaiag@gmail.com>
2021-02-15 15:44:50 -0300

    Formatação de código

--- src/app/adapters/express/ExpressMiddlewareAdapter.js
+++ src/app/adapters/express/ExpressMiddlewareAdapter.js
@@ -1,13 +1,10 @@
 module.exports = function (middleware) {
   return async (req, res, next) => {
-
-    const httpResponse = await middleware.execute(req)
+    const httpResponse = await middleware.execute(req);
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      next()
+      next();
     } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body
-      })
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body })
     }
   }
 }
