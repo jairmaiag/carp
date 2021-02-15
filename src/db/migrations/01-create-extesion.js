dae2d40dde823a124d6bc46eee191fcd66de9c41
Jair <jairmaiag@gmail.com>
2021-02-15 17:45:28 -0300

    Ajsutes no código

--- src/db/migrations/01-create-extesion.js
+++ src/db/migrations/01-create-extesion.js
@@ -1,19 +1,9 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"', { transaction: t })
-      ])
-    })
-  },
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
+    queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"', { transaction: t }),
+  ])),
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.sequelize.query('DROP EXTENSION "uuid-ossp"', { transaction: t })
-      ])
-    })
-  }
+  down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
+    queryInterface.sequelize.query('DROP EXTENSION "uuid-ossp"', { transaction: t }),
+  ])),
 };
