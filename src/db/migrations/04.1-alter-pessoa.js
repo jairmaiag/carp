ac5f2992292bae0c43c492c0785175885564c072
Jair <jairmaiag@gmail.com>
2021-02-17 18:46:22 -0300

    Criação de novas constraints para duplicação de nome

--- src/db/migrations/04.1-alter-pessoa.js
+++ src/db/migrations/04.1-alter-pessoa.js
@@ -1,7 +1,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
     queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ALTER COLUMN "UUIdPes" SET DEFAULT uuid_generate_v4()', { transaction: t }),
-    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesUnique" UNIQUE ("nomePes", "nomeMeioPes", "sobrenomePes")', { transaction: t }),
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesUnique" UNIQUE ("nomePes")', { transaction: t }),
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesNomeMeioPesUnique" UNIQUE ("nomePes", "nomeMeioPes")', { transaction: t }),
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesSobrePesUnique" UNIQUE ("nomePes", "sobrenomePes")', { transaction: t }),
+    queryInterface.sequelize.query('ALTER TABLE carp."Pessoa" ADD CONSTRAINT "nomePesMeioPesSobrePesUnique" UNIQUE ("nomePes", "nomeMeioPes", "sobrenomePes")', { transaction: t }),
   ])),
 
   down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
