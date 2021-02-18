48e5fb7938796fb421eaf05e67e83dd68a63281f
Jair <jairmaiag@gmail.com>
2021-02-18 11:12:11 -0300

    Limpeza e formaçtação de código

--- src/app/route/crudRoute.js
+++ src/app/route/crudRoute.js
@@ -1,17 +1,17 @@
-const adaptRoute = require('../adapters/express/ExpressRouteAdapter')
-const checkUUId = require('../middleware/CheckUUIdMiddlewareFactory')
-const checkId = require('../middleware/CheckIdMiddlewareFactory')
-const checkFilter = require('../middleware/CheckFilterMiddlewareFactory')
-const checkLogin = require('../middleware/CheckLoginMiddleware')
+const adaptRoute = require('../adapters/express/ExpressRouteAdapter');
+const checkUUId = require('../middleware/CheckUUIdMiddlewareFactory');
+const checkFilter = require('../middleware/CheckFilterMiddlewareFactory');
+const checkLogin = require('../middleware/CheckLoginMiddleware');
 
-module.exports = function (app) {
-  app.get('/:model/', checkLogin, checkFilter, adaptRoute(app))
-  app.get('/:model/paginacao', checkLogin, adaptRoute(app))
-  app.get('/:model/:UUId',checkLogin, checkUUId, adaptRoute(app))
- 
-  app.post('/:model/',checkLogin, checkUUId, adaptRoute(app))
+module.exports = (app) => {
+  const model = '/:model/';
+  app.get(`${model}`, checkLogin, checkFilter, adaptRoute(app));
+  app.get(`${model}paginacao`, checkLogin, adaptRoute(app));
+  app.get(`${model}:UUId`, checkLogin, checkUUId, adaptRoute(app));
 
-  app.put('/:model/',checkLogin, checkUUId, adaptRoute(app))
+  app.post(`${model}`, checkLogin, checkUUId, adaptRoute(app));
 
-  app.delete('/:model/:UUId',checkLogin, checkUUId, adaptRoute(app))
-}
+  app.put(`${model}`, checkLogin, checkUUId, adaptRoute(app));
+
+  app.delete(`${model}:UUId`, checkLogin, checkUUId, adaptRoute(app));
+};
