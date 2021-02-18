c19482918362dfb1e2141684a7c9123a8caa3d28
Jair <jairmaiag@gmail.com>
2021-02-18 11:12:30 -0300

    Limpeza e formatação de codigo

--- src/app/adapters/express/ExpressRouteAdapter.js
+++ src/app/adapters/express/ExpressRouteAdapter.js
@@ -1,58 +1,57 @@
 const { badRequest } = require('../../helpers/http/HttpHelpers')
 
-module.exports = function (app) {
+module.exports = (app) => {
   return async (req, res) => {
     try {
-      let rotaNaoEncontrada = false
-      let httpResponse = null
-      const method = req.method.toUpperCase()
-      const url = req.originalUrl
-      const { id } = req.params
-      const { UUId } = req.params
+      let rotaNaoEncontrada = false;
+      let httpResponse = null;
+      const method = req.method.toUpperCase();
+      const url = req.originalUrl;
+      const { id, UUId, model } = req.params;
 
-      const model = req.params.model.charAt(0).toUpperCase() + req.params.model.slice(1)
-      const controller = eval(`new app.src.controller.${model.toLowerCase()}.${model}Controller(app)`)
+      const modelName = model.charAt(0).toUpperCase() + model.slice(1);
+      const controller = eval(`new app.src.controller.${modelName.toLowerCase()}.${modelName}Controller(app)`);
 
       if (method === 'GET') {
         if (UUId) {
-          httpResponse = await controller.findByUUId(req)
+          httpResponse = await controller.findByUUId(req);
         } else if (id) {
-          httpResponse = await controller.findById(req)
+          httpResponse = await controller.findById(req);
         } else if (url.includes('paginacao')) {
-          httpResponse = await controller.findAndPaginate(req)
+          httpResponse = await controller.findAndPaginate(req);
         } else {
-          httpResponse = await controller.index(req)
+          httpResponse = await controller.index(req);
         }
       } else if (method === 'POST') {
-        httpResponse = await controller.insert(req)
+        httpResponse = await controller.insert(req);
       } else if (method === 'PUT') {
-        httpResponse = await controller.update(req)
+        httpResponse = await controller.update(req);
       } else if (method === 'DELETE') {
         if (UUId || id) {
-          httpResponse = await controller.delete(req)
+          httpResponse = await controller.delete(req);
         } else {
-          rotaNaoEncontrada = true
+          rotaNaoEncontrada = true;
         }
       } else {
-        rotaNaoEncontrada = true
+        rotaNaoEncontrada = true;
       }
 
       if (rotaNaoEncontrada) {
-        httpResponse = badRequest('Rota não encontrada')
+        httpResponse = badRequest(req.i18n_texts.path_not_found);
       }
 
       if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-        res.status(httpResponse.statusCode).json(httpResponse.body)
+        res.status(httpResponse.statusCode).json(httpResponse.body);
       } else {
         res.status(httpResponse.statusCode).json({
           error: httpResponse.body
-        })
+        });
       }
     } catch (error) {
       res.status(400).json({
-        mensagem: 'Erro no arquivo ExpressRouteAdapter.',
+        mensagem: req.i18n_texts.error_on_ExpressRouteAdapter,
         error: error.stack
-      })
+      });
     }
   }
-}
+};
