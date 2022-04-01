f8e3067d35c498f6217d0bffd8de6a2613370dd2
Jair M. Diniz <jairmaiag@gmail.com>
2022-04-01 15:33:32 -0300

    Ajustes para criação de banco do sistema.

--- src/controller/main/MainController.js
+++ src/controller/main/MainController.js
@@ -1,10 +1,18 @@
-const { Client } = require("pg")
+const Conection  = require("../../db/config/conection");
 const { serverError, ok } = require('../../app/helpers/http/HttpHelpers')
 
 class MainController {
-
   constructor(app) {
     this.app = app
+    this.bancoCarp = { 
+      usuario: process.env.DATABASEUSERNAME,
+      senha:process.env.DATABASEPASSWORD,
+      host:process.env.DATABASEHOST,
+      porta: process.env.DATABASPORT,
+      banco:process.env.DATABASENAME,
+      esquema: process.env.DATABASESCHEMA
+    }
+ 
   }
 
   index(req, res) {
@@ -25,25 +33,31 @@ class MainController {
       return serverError(error)
     }
   }
-
+  async usuarioAcessoExistente(cliente) {
+    const users = await cliente.query("select usename from pg_user");
+    const existUser = users.find(user => user.usename === this.bancoCarp.banco);
+    return existUser !== undefined;
+  }
   async criarUsuario(cliente) {
     /* Criação do usuário de acesso ao banco */
+    let mensagem = `Usuário de banco já existe. Saindo da criação do usuario. Aguarde a verificação do banco...`;
     try {
-      await cliente.query("select usename from pg_user", (errusu, usus) => {
-        let mensagem = `Usuário de banco já existe. Saindo da criação do usuario. Aguarde a verificação do banco...`;
-        let usubd = usus.rows.filter((usu) => usu.usename === "carp");
-        if (usubd.length > 0) {
-          console.log(mensagem);
+        if (await this.usuarioAcessoExistente(cliente)) {
+          console.info(mensagem);
           return ok(mensagem);
         }
 
         const criarUsuario =
-          `CREATE ROLE carp WITH LOGIN SUPERUSER CREATEDB CREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD 'carp'`;
+          `CREATE ROLE ${this.bancoCarp.banco} WITH LOGIN SUPERUSER CREATEDB CREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD '${this.bancoCarp.banco}'`;
         mensagem = `Criando usuario...`;
-        cliente.query(criarUsuario, (errusu, usu) => {
-          console.log(mensagem);
-        })
-      })
+        console.info(mensagem);
+        const newUser = await cliente.query(criarUsuario);
+        if (await this.usuarioAcessoExistente(cliente)) {
+          console.info('Criação do usuário de banco concluída.');
+          return ok(mensagem);
+        }else{
+          throw new Error('Erro na criação do usuário de acesso ao banco.');
+        }
     } catch (error) {
       return serverError(error)
     }
@@ -51,73 +65,44 @@ class MainController {
 
   async criarBanco(dadosConexao) {
     try {
-      let mensagem = null;
-      const objetoConexao = { connectionString: this.montarStringConexao(dadosConexao), };
-      const cliente = new Client(objetoConexao);
-
-      cliente.connect();
-      const retonoUsuario =  await this.criarUsuario(cliente);
-      if(retonoUsuario.statusCode !== 200){
-        return retonoUsuario;
-      }
+      let mensagem = `Banco já existe. Consultando o schema.`;
+      const conexao = new Conection(this.montarStringConexao(dadosConexao));
+      await this.criarUsuario(conexao);
       /* Criação do banco de dados */
-      cliente.query("SELECT datname FROM pg_database", (err1, res1) => {
-        let bd = res1.rows.filter((banco) => banco.datname == "carp");
-        if (bd.length > 0) {
-          mensagem = `Banco já existe. Saindo da criação de banco de dados.`;
-          console.log(mensagem);
-          cliente.end();
-          return ok(mensagem);
+      const dataBases = await conexao.query('SELECT datname FROM pg_database');
+      const existDataBase = dataBases.find(data => data.datname === this.bancoCarp.banco);
+      let existe = existDataBase !== undefined;
+      if(!existe){
+        const criarBanco = `CREATE DATABASE ${this.bancoCarp.banco} WITH OWNER = ${this.bancoCarp.banco} ENCODING = 'UTF8' CONNECTION LIMIT = -1`;
+        console.info('Criando banco...');
+        await conexao.query(criarBanco);
+        console.info('Criação de banco concluída!');
+      }else{
+        console.info(mensagem);
+      }
+      const conexaoCarp = new Conection(this.montarStringConexao(this.bancoCarp));
+
+      const schemas =  await conexaoCarp.showAllSchemas();
+      if(schemas.length === 0){
+        console.info('Criando schema...');
+        conexaoCarp.createSchema(this.bancoCarp.esquema);
+      }else{
+        const existSchema = schemas.find(data => data === this.bancoCarp.banco);
+        if(existSchema === undefined){
+          console.info('Criando schema...');
+          conexaoCarp.createSchema(this.bancoCarp.esquema);
+        }else{
+          console.info('Schema já criado...');
         }
-
-        const criarBanco = `CREATE DATABASE carp WITH OWNER = carp ENCODING = 'UTF8' CONNECTION LIMIT = -1`;
-
-        cliente.query(criarBanco, (errdb, db) => {
-          console.log('Criando banco...')
-          cliente.end()
-          console.log('Criação de banco concluída!')
-        })
-      })
-
-      /* Criação do schema do banco 
-      CREATE SCHEMA carp AUTHORIZATION carp;
-      GRANT ALL ON SCHEMA carp TO carp;
-      ALTER DEFAULT PRIVILEGES IN SCHEMA carp GRANT ALL ON TABLES TO carp;
-
-      */
-      cliente.query("select schema_name from information_schema.schemata", (err1, res1) => {
-        let bd = res1.rows.filter((banco) => banco.schema_name == "carp")
-        if (bd.length > 0) {
-          mensagem = 'Schema do banco já existe. Saindo da criação de schema.'
-          console.log(mensagem)
-          cliente.end()
-          return ok(mensagem)
-        }
-
-        const criarSchema =
-          "CREATE SCHEMA carp AUTHORIZATION carp"
-
-        cliente.query(criarSchema, (errdb, db) => {
-          console.log('Criando Schema...')
-          cliente.end()
-          console.log('Criação de schema concluída!')
-        })
-
-        cliente.query('GRANT ALL ON SCHEMA carp TO carp', (errdb, db) => {
-          console.log('Privilegios para o Schema...')
-          cliente.end()
-        })
-        cliente.query('ALTER DEFAULT PRIVILEGES IN SCHEMA carp GRANT ALL ON TABLES TO carp', (errdb, db) => {
-          console.log('Privilegios para o Schema...')
-          cliente.end()
-        })
-
-      })
-      return ok('Criação do banco concluída!')
+      }
+      
+      await conexaoCarp.close();
+      await conexao.close();
+      return ok({mensagem:'Criação de banco concluída. Verifique também o console do nodejs para ver os resultados.', data: this.bancoCarp});
     } catch (error) {
       return serverError(error)
     }
   }
 }
 
-module.exports = () => MainController
+module.exports = () => MainController;
