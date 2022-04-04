5029a4e91f78dbe77bcd8403319a0c9322fb6680
Jair M. Diniz <jairmaiag@gmail.com>
2022-04-04 16:25:35 -0300

    captura do parâmetro do modo de trabalho

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 /* importar as configurações do servidor */
-var app = require('./src/app/server');
+const app = require('./src/app/server');
+/* Captura da passgem de parâmetros via linha de comando */
+const param = process.argv;
+if(param.length > 2){
+  const modo = param[2];
+  if(modo === 'production'){
+    process.env.NODE_ENV = 'production';
+  }
+}
 
 /* parametrizar a porta de escuta */
 let port = process.env.PORT || 80
 app.listen(port, function () {
-  let acesso = "Acesse http://localhost" + (port == 80 ? "" : ":" + port)
+  let acesso = "http://localhost" + (port == 80 ? "" : ":" + port)
   console.log(acesso)
   console.log(`Para criar o banco acesse ${acesso}/criarbanco e siga as instruções.\n`)
 });
