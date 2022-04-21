404b527b653c9255ed313d2acb52d61d28988734
Jair M. Diniz <jairmaiag@gmail.com>
2022-04-21 18:26:14 -0300

    Rota principal

--- src/app/route/MainRoute.js
+++ src/app/route/MainRoute.js
@@ -24,18 +24,16 @@ module.exports = (app) => {
     let httpResponse = controller.index(req, res);
 
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      res.status(httpResponse.statusCode).json(httpResponse.body)
+      res.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body
-      })
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body });
     }
   })
 
   app.get("/criarbanco", function (req, res) {
     res.status(200).send(
       'Passe os dados utilizando o method POST, no formato json como abaixo: <br />{<br />"host":"enderecoBanco", <br />"porta":5432, <br />"banco":"postgres",<br />"usuario":"postgres", <br />"senha":"senhaBanco" <br />} <br />Substituindo os dados para a conexão com o banco padrão do Postgres.')
-  })
+  });
 
   app.post("/criarbanco", async function (req, res) {
     if (!validaCampos(req.body)) {
@@ -43,17 +41,15 @@ module.exports = (app) => {
         mensagem:
           "Formato esperado como no exemplo: {\"host\":'enderecoBanco', 'porta':5432,'banco':'postgres','usuario':'postgres','senha':'senhaBanco'} - Substituindo as aspas simples por aspas duplas.",
       })
-      return
+      return;
     }
 
-    httpResponse = await controller.criarBanco(req.body)
+    const httpResponse = await controller.criarBanco(req.body);
 
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      res.status(httpResponse.statusCode).json('Criação de banco concluida. Verifique também o console do nodejs para ver os resultados.')
+      res.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body
-      })
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body });
     }
-  })
+  });
 }
