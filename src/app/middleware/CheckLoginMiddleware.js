0bb583231d0ce0d23ba5471ca230598295f43ae2
Jair M. Diniz <jairmaiag@gmail.com>
2022-04-22 19:30:07 -0300

    Login logout

--- src/app/middleware/CheckLoginMiddleware.js
+++ src/app/middleware/CheckLoginMiddleware.js
@@ -1,13 +1,36 @@
+const jwt = require('jsonwebtoken');
+const forJWT = (req, res) => {
+  const token = req.headers['x-access-token'];
+
+  if (token === undefined) {
+    res.status(401).json({ mensagem: `${req.i18n_texts.user_not_logged_in} ${req.i18n_texts.please_do_login}` });
+    return null;
+  }
+
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(500).json({ auth: false, mensagem: `${req.i18n_texts.user_not_logged_in} ${req.i18n_texts.please_do_login}` });
+    }
+
+    // se tudo estiver ok, salva no request para uso posterior
+    req.userId = decoded.id;
+    return req;
+  });
+}
+const forSession = (req, res) => {
+  if (req.session.usuario === undefined) {
+    res.status(401).json({ mensagem: `${req.i18n_texts.user_not_logged_in} ${req.i18n_texts.please_do_login}` });
+  }
+  return res;
+}
 const CheckLoginMiddleware = (req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
     const { originalUrl } = req;
     if (originalUrl === '/' || originalUrl === '/login') {
       return next();
     }
-    if (req.session.usuario === undefined) {
-      res.status(401).json({ mensagem: `${req.i18n_texts.user_not_logged_in} ${req.i18n_texts.please_do_login}` });
-      return null;
-    }
+    // req = forJWT(req, res);
+    req = forSession(req, res)
   }
   next();
 };
--- src/controller/acesso/AcessoController.js
+++ src/controller/acesso/AcessoController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const repository = require('../../db/repository/UsuarioRepository')
 const { serverError, badRequest, ok } = require('../../app/helpers/http/HttpHelpers')
 
@@ -14,10 +15,17 @@ class AcessoController {
         return badRequest(`${req.i18n_texts.field_login_password_required} ${req.i18n_texts.please_try_again}`)
       }
 
-      const entity = await repository.findByLoginSenha(filter)
+      const entity = await repository.findByLoginSenha(filter);
       if (!entity) {
         return badRequest(`${req.i18n_texts.username_password_invalid} ${req.i18n_texts.please_try_again}`)
       }
+      const UUId = entity.UUId;
+
+      const token = jwt.sign({ UUId }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+      entity.dataValues.auth=true;
+      entity.dataValues.token=token;
 
       return ok(entity)
     } catch (error) {
@@ -36,13 +44,13 @@ class AcessoController {
 
   async alterarsenha(req) {
     try {
-      if(!req.body.UUId){
+      if (!req.body.UUId) {
         return badRequest(`${req.i18n_texts.UUId_required}`);
       }
-      if(!req.body.login || !req.body.senha){
+      if (!req.body.login || !req.body.senha) {
         return badRequest(`${req.i18n_texts.field_login_password_required}`);
       }
-      if(!req.body.senhaAntiga){
+      if (!req.body.senhaAntiga) {
         return badRequest(`${req.i18n_texts.field_login_oldpassword_required}`);
       }
       const entity = await repository.update(req.body);
